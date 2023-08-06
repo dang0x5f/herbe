2ee620df95929789aa0550996b22151673021f7e
dang0x5f <dannypgehrz@gmail.com>
2023-08-06 05:19:02 -0500

    my herbe fork

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
-static const char *background_color = "#3e3e3e";
-static const char *border_color = "#ececec";
-static const char *font_color = "#ececec";
-static const char *font_pattern = "monospace:size=10";
+static const char *background_color = "#282a36";
+static const char *border_color = "#bd93f9";
+static const char *font_color = "#f8f8f2";
+static const char *font_pattern = "xft:Deja Vu Sans Mono:weight=bold:size=8";
 static const unsigned line_spacing = 5;
 static const unsigned int padding = 15;
 
-static const unsigned int width = 450;
+static const unsigned int width = 250;
 static const unsigned int border_size = 2;
 static const unsigned int pos_x = 30;
 static const unsigned int pos_y = 60;
@@ -13,7 +13,7 @@ static const unsigned int pos_y = 60;
 enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
 enum corners corner = TOP_RIGHT;
 
-static const unsigned int duration = 5; /* in seconds */
+static const unsigned int duration = 3; /* in seconds */
 
 #define DISMISS_BUTTON Button1
 #define ACTION_BUTTON Button3
